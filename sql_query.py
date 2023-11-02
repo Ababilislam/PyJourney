2c37962d2c8aa3242fbd1195ad98cca7a6e5bbe7
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-11-02 20:06:07 +0600

    query edited

--- sql_query.py
+++ sql_query.py
@@ -13,14 +13,14 @@ mycursor = mydb.cursor()
 #     print(i)
 
 # sql_query = "select * from customers limit 5 offset 3"
-# # adr = ("Yellow garden 2",)
-# # mycursor.execute(sql_query,adr)
-# mycursor.execute(sql_query)
-# my_result = mycursor.fetchall()
-# for x in my_result:
-#     print(x)
-# mydb.commit()
-# print(mycursor.rowcount, "record(s) affected")
+# adr = ("Yellow garden 2",)
+# mycursor.execute(sql_query,adr)
+mycursor.execute(sql_query)
+my_result = mycursor.fetchall()
+for x in my_result:
+    print(x)
+mydb.commit()
+print(mycursor.rowcount, "record(s) affected")
 
 sql_query = ("select user.name as user, product.name as favorite\
              from users \
