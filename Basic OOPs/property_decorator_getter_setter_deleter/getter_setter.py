db2f3ce2a1ada85e4bb70df1386aad3c0483f5cb
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-11-21 14:25:24 +0600

    getter setter

--- Basic OOPs/property_decorator_getter_setter_deleter/getter_setter.py
+++ Basic OOPs/property_decorator_getter_setter_deleter/getter_setter.py
@@ -1,1 +1,30 @@
 
+class Celcius:
+    def __init__(self, temperature=0):
+        self.temperature = temperature
+
+    def to_furenhite(self):
+        return (self.temperature*1.8)+32
+
+    #getter
+    def get_temperature(self):
+        print("getting value")
+        return self._temperature
+
+    #setter
+    def set_temperature(self, value):
+        print("setting vlaue")
+        if value < -273.15:
+            raise ValueError("temperature blew  negetive 273 is not possible")
+        self._temperature = value
+
+
+    temperature = property(get_temperature,set_temperature)
+
+
+human =Celcius(37)
+
+print(human.temperature)
+print(human.to_furenhite())
+human.temperature=-300
+print(human.to_furenhite())
