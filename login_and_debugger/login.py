2ee181bd65b2ccf51b4f0bb8134971f0d2d6e9cc
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-11-24 17:39:12 +0600

    python full feature logger

---
+++ login_and_debugger/login.py
@@ -0,0 +1,30 @@
+import datetime
+import logging
+
+# logging.debug("this is a debug message")
+# logging.info("this is a info message")
+# logging.warning("this is a worning message")
+# logging.error("this is a error message")
+# logging.critical("this is a critical mmessage")
+
+
+# logging.basicConfig(level=logging.DEBUG)
+# logging.debug('This will get logged')
+
+import logging
+import time
+# logging.basicConfig(filename='app.log',filemode='w',format='%(name)s - %(levelname)s - %(message)s :- %(asctime)s')
+# logging.warning("Admin Login")
+
+
+
+# -------------proper log---------------
+
+# logging.basicConfig(filename='app.log',filemode='w',format="name:- %(name)s :: level:- %(levelname)s :: message:- %(message)s :: %(asctime)s", datefmt='date:- %d-%m-%y time:- %H:%M:%S')
+# # logging.warning('this is system generated warning')
+# name = "ab"
+# logging.error(f"{name} raised and error ")
+
+#----------------end proper log---------
+
+
