04223de39d281dff78d559aedcfae09c27b7f747
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-11-27 21:14:18 +0600

    update age calculator

--- exceptions/age_claculator_abstrct.py
+++ exceptions/age_claculator_abstrct.py
@@ -21,14 +21,14 @@ class Student(Person):
     def age(self):
         today =str(datetime.today().date())
         # # print(type(today))
-        print(today)            #year-month-day
+        # print(today)            #year-month-day
         date_of_birth = self.dob
         # # print(type(date_of_birth))
-        print(date_of_birth)
+        # print(date_of_birth)
         start_date = datetime.strptime(date_of_birth, '%Y-%m-%d')
         end_date = datetime.strptime(today, "%Y-%m-%d")
         date_delta = relativedelta.relativedelta(end_date,start_date)
-        print(date_delta.years, 'Years,', date_delta.months, 'months,', date_delta.days, 'days')
+        print("your age :", date_delta.years, 'Years,', date_delta.months, 'months,', date_delta.days, 'days')
 
 
 s1 = Student("ab", '2000-2-29')
