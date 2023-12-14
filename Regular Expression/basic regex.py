0fdfbc6cd96b63b548ca5766b95d5c733bb408d8
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-12-14 18:04:49 +0600

    updated re bracks

--- Regular Expression/basic regex.py
+++ Regular Expression/basic regex.py
@@ -1,8 +1,13 @@
 import re
 
-pattern = '^a...s$'
-test_string = "abkls"
+# pattern = '^a...s$'
+# test_string = "abkls"
+# pattern = 'a{2,3}'
+# test_string = "aabc daaat"
+pattern = '[0-9]{2,4}'
+test_string = "12 and 345673	"
 result = re.match(pattern,test_string)
+print(result)
 if result:
     print("search successfull")
 else:
